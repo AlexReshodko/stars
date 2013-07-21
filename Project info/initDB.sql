af3c4d8a8559a6c525b6db2d1739452ffab20955
MaZaTroN <mazon4uk@bigmir.net>
2013-07-21 16:15:50 +0300

    Init SQL script

--- Project info/initDB.sql
+++ Project info/initDB.sql
@@ -7,3 +7,30 @@ CREATE TABLE `users` (
 	`role`  varchar(255) NULL ,
 	PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `exchanges` (
+	`id`  int NOT NULL ,
+	`from_uid`  int NULL ,
+	`to_uid`  int NULL ,
+	`comment`  varchar(255) NULL ,
+	`stars`  int NULL ,
+	`date`  datetime NULL ,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `level_system` (
+	`id`  int NOT NULL ,
+	`work_time`  int NULL ,
+	`stars_num`  int NULL ,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `profiles` (
+	`id`  int NOT NULL ,
+	`user_id`  int NULL ,
+	`name`  varchar(255) NULL ,
+	`surname`  varchar(255) NULL ,
+	`phone`  varchar(255) NULL ,
+	`about`  varchar(255) NULL ,
+	PRIMARY KEY (`id`)
+);
